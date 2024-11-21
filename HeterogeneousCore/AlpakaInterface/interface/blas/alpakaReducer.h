d7368c9b6b39090b412cf953f57b18b19fe42d5b
Alexei Strelchenko <astrel@fnal.gov>
2024-11-21 09:11:05 -0600

    removed artifacts, fixed default warp sizes , added one method to initialize vector array

--- HeterogeneousCore/AlpakaInterface/interface/blas/alpakaReducer.h
+++ HeterogeneousCore/AlpakaInterface/interface/blas/alpakaReducer.h
@@ -6,6 +6,13 @@ namespace cms::alpakatools {
 
     template <typename T> constexpr std::enable_if_t<std::is_arithmetic_v<T>, T> zero() { return static_cast<T>(0); }
 
+    template <typename T, std::int32_t N> ALPAKA_FN_HOST_ACC  inline VecArray<T, N> zero() {
+      VecArray<T, N> v;
+      CMS_UNROLL_UNROLL
+      for (std::int32_t i = 0; i < N; i++) v[i] = zero<T>();
+      return v;
+    }
+
     template <typename T, typename U> constexpr std::enable_if_t<std::is_arithmetic_v<T> and std::is_arithmetic_v<U>, T> set(U x) { return static_cast<T>(x); }
 
     /**
