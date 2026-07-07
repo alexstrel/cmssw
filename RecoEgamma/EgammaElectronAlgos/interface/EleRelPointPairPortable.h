c907a1b886f15012b454b85cd413bfee46e8e021
Alexei Strelchenko <astrel@fnal.gov>
2026-07-07 15:07:14 -0500

    renamed methods following ROOT conventions

--- RecoEgamma/EgammaElectronAlgos/interface/EleRelPointPairPortable.h
+++ RecoEgamma/EgammaElectronAlgos/interface/EleRelPointPairPortable.h
@@ -63,42 +63,11 @@ namespace egamma {
 
     template <typename TAcc>
     ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE T dPerp(TAcc const& acc) const {
-      const T relP1_2dnorm = relP1.template partial_norm<TAcc>(acc);
-      const T relP2_2dnorm = relP2.template partial_norm<TAcc>(acc);
+      const T relP1_rho = relP1.rho(acc);
+      const T relP2_rho = relP2.rho(acc);
 
-      return (relP1_2dnorm - relP2_2dnorm);
+      return (relP1_rho - relP2_rho);
     }
-
-    // Helper function to compute relative position
-    //constexpr Vec3 relativePosition(const Vec3& point, const Vec3& origin) const {
-    //return cms::alpakatools::math::xmy(point, origin);
-    //}
-
-    // Calculate  relative eta
-    //template <typename TAcc>
-    //ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE T relative_eta(TAcc const& acc, const Vec3& p, const Vec3& origin) const {
-    //const T tmp = cms::alpakatools::math::diff_norm2(p, origin);
-    //const T pdiff = alpaka::math::sqrt(acc, tmp);
-    //const T z = p[2] - origin[2];
-
-    //return 0.5 * alpaka::math::log(acc, (pdiff + z) / (pdiff - z));
-    //}
-
-    // Calculate relative phi
-    //template <typename TAcc>
-    //ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE T relative_phi(TAcc const& acc, const Vec3& p1, const Vec3& p2) const {
-    //const T phi = alpaka::math::atan2(acc, p1[1], p1[0]) - alpaka::math::atan2(acc, p2[1], p2[0]);
-    //return reduceRange(acc, phi);
-    //}
-
-    // Normalize phi to the range [-pi, pi]
-    //template <typename TAcc>
-    //ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE T reduceRange(TAcc const& acc, const T x) const {
-    //constexpr T o2pi = 1. / (2. * M_PI);
-    //if (alpaka::math::abs(acc, x) <= T(M_PI))
-    //return x;
-    //return x - alpaka::math::floor(acc, x * o2pi + (x < 0 ? -0.5 : 0.5)) * 2. * M_PI;
-    //}
 
     template <typename TAcc>
     ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE auto dPhi(TAcc const& acc) const {
